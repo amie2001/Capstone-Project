814c3dc2c4e88ada5b34d18a90a8a4e3e15f2b1e
pavi2001 <74369749+amie2001@users.noreply.github.com>
2024-03-20 11:58:31 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin do
 if [[ $GIT_BRANCH == "origin/dev" ]]; then 
 docker tag capstone  pavi2244/dev
 docker push pavi2244/dev
-elif [[ $GIT_BRANCH == "main" ]]; then 
+elif [[ $GIT_BRANCH == "origin/master" ]]; then 
 docker tag capstone  pavi2244/prod
 docker push pavi2244/prod
 fi 
