b550b93aaba51c8542adf4e6c3aa76f7330d2c37
pavi2001 <74369749+amie2001@users.noreply.github.com>
2024-03-20 11:40:20 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,12 @@
 # Log in to Docker Hub
 echo "Logging in to Docker Hub..."
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin docker.io
+if [ $GIT_BRANCH == "dev" ]; then 
+docker tag capstone  pavi2244/dev
+docker push pavi2244/dev
+elif [ $GIT_BRANCH == "main" ]; then 
+docker tag capstone  pavi2244/prod
+docker push pavi2244/prod
+fi 
 
-# Push the Docker image to Docker Hub
-echo "Pushing Docker image to Docker Hub..."
-docker push "$DOCKER_CAPSTONE_IMAGE"
 
-echo "Deployment to Docker Hub completed."
