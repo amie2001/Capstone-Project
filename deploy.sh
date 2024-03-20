d945201c2a7fc34b75f35c7e09408f03266d04ea
pavi2001 <74369749+amie2001@users.noreply.github.com>
2024-03-20 11:46:33 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 # Log in to Docker Hub
 echo "Logging in to Docker Hub..."
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin docker.io
-if [ $GIT_BRANCH == "dev" ]; then 
+if [[ $GIT_BRANCH == "dev" ]]; then 
 docker tag capstone  pavi2244/dev
 docker push pavi2244/dev
-elif [ $GIT_BRANCH == "main" ]; then 
+elif [[ $GIT_BRANCH == "main" ]]; then 
 docker tag capstone  pavi2244/prod
 docker push pavi2244/prod
 fi 
